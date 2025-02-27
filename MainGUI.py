5aa27a29d06d28997965c98de276bf8abede64b5
Santiago Echavarria <37352122+sechava4@users.noreply.github.com>
2025-02-26 23:48:36 -0500

    Final inertia value with new roller

--- MainGUI.py
+++ MainGUI.py
@@ -258,8 +258,7 @@ class App(QMainWindow, Ui_mainWindow):
         self.lcdAFR.display(afr)
         current_time = time.time()
         rpm_rod = pulsos * 0.3  # (n_pulsos / 0.2 sec)  * (1 rev / 1000 pulsos) * (60 sec / 1 min)  = 0.3 * pulsos. 
-        # hz_rod_temp = (rpm_rod * math.pi) / 30 # (rpm_rod * 2 * math.pi) / 60
-        hz_rod_temp = rpm_rod * 0.010610329
+        hz_rod_temp = (rpm_rod * math.pi) / 30 # (rpm_rod * 2 * math.pi) / 60
 
         if self.setting_flag:
             # Se calcula la relacion de reducción
@@ -336,7 +335,7 @@ class App(QMainWindow, Ui_mainWindow):
                 # Calculos
                 delta_w = hz_rod - self.old_hz
                 acc = delta_w / self.delta_t
-                inertia_total = 3.15882
+                inertia_total = 3.015882
                 # Se calcula el torque en el rodillo con sumatoria de torques
                 '''
                 roller_load = (80 + 100) * 9.81  # Equivalent Load on the bearing
@@ -350,7 +349,7 @@ class App(QMainWindow, Ui_mainWindow):
                 '''
                 chain_friction = 50 * acc  # 2 % of transmission loss
                 # torque_rod_temp = (inertia_total * 1000 * acc) + chain_friction + 0.0662  # + fr_bearings Nm
-                torque_rod_temp = (3158 * acc) + chain_friction + 0.0662  # + fr_bearings Nm
+                torque_rod_temp = (3015 * acc) + chain_friction + 0.0662  # + fr_bearings Nm
 
                 # fir filter
                 torque_rod = torque_rod_temp * 0.8 + self.old_torque_rod * 0.1 + self.old_torque_rod2 * 0.1
